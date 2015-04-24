ffe48c53d424fcb099452a47526cdb938014b3ee
Nathalie <Nathalie@172.29.41.94>
2015-04-24 14:38:56 +0200

    Avancement gallerie (upload img)

--- src/Site/TrailBundle/Controller/GalleryController.php
+++ src/Site/TrailBundle/Controller/GalleryController.php
@@ -108,10 +108,11 @@ class GalleryController extends Controller
         }
     }
     
-    public function categorieSuppressionAction(Request $request, $idCategorie)
+    public function categorieSuppressionAction(Request $request)
     {
-       /* if($request->isXmlHttpRequest())
+        if($request->isXmlHttpRequest() && $this->getUser()->getRole()->getId() == 1)
         {
+            $idCategorie = $request->request->get('idCategorie', '');
             $manager=$this->getDoctrine()->getManager();
 
             //On récupère l'objet catégorie
@@ -140,6 +141,162 @@ class GalleryController extends Controller
         else
         {
             throw new NotFoundHttpException('Impossible de trouver la page demandée');
-        }*/
+        }
+    }
+    
+    public function CategorieModifAction(Request $request)
+    {
+        if($request->isXmlHttpRequest() && $this->getUser()->getRole()->getId() == 1)
+        {      
+            $idCategorie = $request->request->get('idCategorie', '');
+            
+            $manager=$this->getDoctrine()->getManager();
+            $repository=$manager->getRepository("SiteTrailBundle:Categorie");        
+            $maCategorie = $repository->findOneById($idCategorie);
+
+            $formBuilder = $this->get('form.factory')->createBuilder('form', $maCategorie);
+            $formBuilder
+                    ->setAction($this->generateUrl('site_trail_category_modification'))
+                    ->add('label', 'text', array('max_length' => 255,
+                                                    'data' => $maCategorie->getLabel()));
+
+            $form = $formBuilder->getForm();
+            $form->handleRequest($request);
+
+            if ($form->isValid()) 
+            {
+                $manager = $this->getDoctrine()->getManager();
+                $manager->flush();
+                $request->getSession()->getFlashBag()->add('notice', 'Catégorie ajouté');
+
+                return $this->redirect($this->generateUrl('site_trail_gallery'));
+            }
+            
+            $formulaire = $this->get("templating")->render("SiteTrailBundle:Gallery:modifCategoryForm.html.twig", array(
+                                                                'categorie' => $maCategorie,
+                                                                'form' => $form->createView()
+                                                            ));
+
+            return new Response($formulaire);
+        }
+        else
+        {
+            throw new NotFoundHttpException('Impossible de trouver la page demandée');
+        }
+    }
+    
+    public function showAddPictureAction()
+    {
+        //Création du select contenant les catégories
+        $manager = $this->getDoctrine()->getManager();
+        $repository = $manager->getRepository("SiteTrailBundle:Categorie"); 
+        $listeCategorie = $repository->findAll();
+        
+        $selectCategorie = '<div class="form-group">';
+        $selectCategorie .= '<div class="row">';
+        $selectCategorie .= '<label class="col-sm-3 control-label">Catégorie :</label>';
+        $selectCategorie .= '<div class="col-sm-9">';
+        $selectCategorie .= '<select name="categorie" class="form-control">';
+        foreach($listeCategorie as $uneCategorie)
+        {
+            $selectCategorie .= '<option value="'.$uneCategorie->getId().'">'.$uneCategorie->getLabel().'</option>';
+        }
+        $selectCategorie .= '</select>';
+        $selectCategorie .= '</div>';
+        $selectCategorie .= '</div>';
+        $selectCategorie .= '</div>';
+
+        $formulaire = $this->get("templating")->render("SiteTrailBundle:Gallery:formAddPicture.html.twig", array(
+                                                            'selectCategorie' => $selectCategorie
+        ));
+
+        return new Response($formulaire);
+    }
+    
+    public function addPictureAction(Request $request)
+    {
+        var_dump($_POST);
+        var_dump($_FILES);
+        
+        //Sauvegarde du fichier   
+        //$target_dir = "C:/testUp/";
+        $target_dir = "/var/www/uploads";
+        $target_file = $target_dir . basename($_FILES["fichier"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+        
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fichier"]["tmp_name"]);
+            if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "Le fichier n'est pas une image.";
+                $uploadOk = 0;
+            }
+        }
+        
+        //On vérifie la taille du fichier
+        if ($_FILES["fichier"]["size"] > 5000000) {
+            echo "L'image est trop volomineuse.";
+            $uploadOk = 0;
+        }
+        
+        //Autorisation de certaines extensions de fichier
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            echo "Seules les extensions JPG, JPEG, PNG & GIF sont autorisées.";
+            $uploadOk = 0;
+        }
+        
+        //On vérifie qu'il n'y a pas eu d'erreurs lors de l'upload
+        if ($uploadOk == 0)
+        {
+            echo "Il y a eu un problème lors de l'envoi du fichier.";
+        }
+        else
+        {
+            $date = new \DateTime;
+            $newFile = $target_dir."image".date_format($date, 'U').".".$imageFileType;
+                
+            if (move_uploaded_file($_FILES["fichier"]["tmp_name"], $newFile)) {
+                echo "Le fichier a bien été envoyé.";
+                list($width, $height, $type, $attr) = getimagesize($newFile); 
+                
+                $manager = $this->getDoctrine()->getManager();
+                
+                //On rajoute l'image dans la base de données                
+                $titre = $request->request->get('titre', '');
+                $description = $request->request->get('description', '');
+                $poids = $_FILES["fichier"]["size"];
+                $taille = $height.'x'.$width;
+                $auteur = $this->getUser();
+                $repository = $manager->getRepository("SiteTrailBundle:Categorie");
+                
+                var_dump($request->request->get('categorie', ''));
+                
+                $categorie = $repository->findOneById($request->request->get('categorie', ''));
+                $path = $newFile; 
+                
+                $repository = $manager->getRepository("SiteTrailBundle:Image");
+                $newImage = new Image();
+                $newImage->setTitre($titre);
+                $newImage->setDescription($description);
+                $newImage->setPoids($poids);
+                $newImage->setTaille($taille);
+                $newImage->setAuteur($auteur);
+                $newImage->setCategorie($categorie);
+                $newImage->setPath($path);
+
+                $manager->persist($newImage);
+                $manager->flush();
+                
+            } else {
+                echo "Il y a eu un problème lors de l'envoi du fichier.";
+            }
+        }
+        
+        return new Response('OK');
     }
 }    
--- src/Site/TrailBundle/Resources/js/gallery/gallery.js
+++ src/Site/TrailBundle/Resources/js/gallery/gallery.js
@@ -30,7 +30,7 @@ function envoiFormAjout()
     });
 }
 
-/*//Afficher le modal de confirmation de suppression de catégorie
+//Afficher le modal de confirmation de suppression de catégorie
 function supprCategorieConfirm()
 {
     $("#modalAvertissement").modal('show');
@@ -48,4 +48,54 @@ function suppressionCategorie(idCategorie)
             document.location.href=Routing.generate('site_trail_gallery');
         }
     });
-}*/
+}
+
+//Modification d'une catégorie
+function modifCategorie(idCategorie)
+{
+    $('#modalModifCategoryForm').children().remove();
+    $('#modalModifCategoryForm').remove();
+    
+    $.ajax({
+        type: "POST",
+        url: Routing.generate('site_trail_category_modification'),
+        data: {"idCategorie" : idCategorie},
+        cache: false,
+        success: function(data){
+            $("body").append(data);
+            $("#modalModifCategoryForm").modal('show');
+        }
+    });
+}
+
+//Envoi du formulaire de modification
+function envoiFormModif()
+{
+    var data = $('#modifierCategory').serialize();
+    
+    $.ajax({
+        type: "POST",
+        url: Routing.generate('site_trail_category_modification'),
+        cache: false,
+        data: data,
+        success: function(){
+            document.location.href=Routing.generate('site_trail_gallery')
+        }
+    });
+}
+
+$( "body" ).on( "click", "#addPicture", function()
+{
+    $("#modalAddPictureForm").children().remove();
+    $("#modalAddPictureForm").remove();
+
+    $.ajax({
+        type: "POST",
+        url: Routing.generate('site_trail_picture_showAdd'),
+        cache: false,
+        success: function(data){
+            $('body').append(data);
+            $("#modalAddPictureForm").modal('show');
+        }
+    });
+});
